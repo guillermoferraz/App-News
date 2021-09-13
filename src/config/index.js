882c2e93956a8873f3b5318cb3cf33f270e74d71
Guillermo Ferraz <69995581+guillermoferraz@users.noreply.github.com>
2021-09-13 10:42:08 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
     API_HOST:'bing-news-search1.p.rapidapi.com',
-    API_KEY:'2f826cd47amsh35e230935fbd14fp177bedjsnb4e622d01635',
+    API_KEY:'your_api_key',
     URI:'https://bing-news-search1.p.rapidapi.com/news/search'
 }
